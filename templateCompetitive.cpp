db60a050fab772c4f009a937a7a5b055c0c27d0f
Dannie1o1 <daniel.ben.gordon@gmail.com>
2021-06-03 21:01:31 -0400

    Added Templates and Draft Traversal Algos

---
+++ templateCompetitive.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+using namespace std;
+
+int main() 
+{
+//Fast I/O
+ios::sync_with_stdio(0);
+cin.tie(0);
+
+//File I/O
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+
+// solution comes here
+}
